642960ba6dcf55060296f78d607ef1a62e3aa795
tudor-p <jq.tudor@gmail.com>
2020-04-25 12:29:13 +0100

    25/04/2020

--- chapter-4-arrays-objects.js
+++ chapter-4-arrays-objects.js
@@ -72,12 +72,117 @@ let sentence = "Secretarybirds specialize in stomping"
 let words = sentence.split(" ")
 console.log(words);
 // → ["Secretarybirds", "specialize", "in", "stomping"]
-console.log(words.join(". "));
+console.log(words.join(". "))
 // → Secretarybirds. specialize. in. stomping
 
 // .repeat()
-console.log("LA".repeat(3));
+// console.log("LA".repeat(3));
 // → LALALA
 
+// function max(...numbers) {
+//     let result = -Infinity
+//     for (let number of numbers) {
+//       if (number > result) result = number
+//     }
+//     return result
+//   }
+//   console.log(max(4, 1, 9, -2))
 
 
+// Exercises
+
+console.log("\n \n")
+
+function range (start, end, step = 1) {
+    let result = []
+
+    if (step <= 0) {
+        result = "Can only accept positive numbers"
+        return result
+    }
+    
+    if (start <= end) {   
+        while (start <= end) {
+            result.push(start)
+            start += step
+        } 
+    } else {
+        while (start >= end) {
+            result.push(start)
+            start -= step
+        }
+    }
+    return result
+}
+
+
+function sum (...numbers) {
+    let result = 0
+    for (let number of numbers) {
+        result += number 
+    }
+    return result
+}
+
+function reverseArray (arr) {
+    let result = [], i = arr.length
+    while (i > 0) {
+        result.push(arr[i-1])
+        i--
+    }
+    return result
+}
+// console.log(reverseArray([1,2,3,4,5]))
+
+function reverseArrayInPlace (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let old = arr[i]
+        arr [i] = arr [arr.length-i-1]
+        arr [arr.length-i-1] = old
+        if (i > arr.length-i-1 ) break
+    }
+    return arr
+}
+
+//console.log (reverseArrayInPlace([1,2,3,4,5]))
+
+let list = {
+    value: 1, 
+    rest: {
+        value: 2,
+        rest: {
+            value: 3,
+            rest: null
+        }
+    }
+}
+
+function arrayToList(array) {
+    let list = null
+    for (let i = array.length - 1; i >= 0; i--) {
+      list = {value: array[i], rest: list}
+    }
+    return list
+  }
+
+  
+  function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+      array.push(node.value);
+    }
+    return array;
+  }
+  
+//   function prepend(value, list) {
+//     return {value, rest: list};
+//   }
+  
+//   function nth(list, n) {
+//     if (!list) return undefined;
+//     else if (n == 0) return list.value;
+//     else return nth(list.rest, n - 1);
+//   }
+// }
+
+console.log(arrayToList([1,2,3,4]))
